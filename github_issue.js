71b12cc5a79b6150bdd22f25e71871203249c4ad
pppurple <shuggie.otis0@gmail.com>
2016-09-01 03:27:07 +0900

    Use spaces indent instead of tab.

--- github_issue.js
+++ github_issue.js
@@ -9,96 +9,96 @@ var nunjucks  = require('nunjucks');
 nunjucks.configure('views', { autoescape: true });
 
 var github = new GitHubApi({
-	version: "3.0.0",
-	debug: false
+  version: "3.0.0",
+  debug: false
 });
 
 github.authenticate({
-	type: "basic",
-	username: "your github username",
-	password: "your github password"
+  type: "basic",
+  username: "your github username",
+  password: "your github password"
 });
 
 var options = {
-	user : 'jjug-ccc',
-	repo : 'call-for-paper-2016fall',
-	state: 'open',
-	per_page: 100
+  user : 'jjug-ccc',
+  repo : 'call-for-paper-2016fall',
+  state: 'open',
+  per_page: 100
 };
 
 exports.createServer = function() {
 
-	var server = http.createServer();
+  var server = http.createServer();
 
-	server.on('request', function(req, res) {
-		if(req.url == '/'){
-			index(res);
-		}else if(path.extname(req.url) == '.js'){
-			static(req, res);
-		}else if(path.extname(req.url) == '.css'){
-			static(req, res);
-		}else if(path.extname(req.url) == '.png'){
-			static(req, res);
-		}
-	});
+  server.on('request', function(req, res) {
+    if(req.url == '/'){
+      index(res);
+    }else if(path.extname(req.url) == '.js'){
+      static(req, res);
+    }else if(path.extname(req.url) == '.css'){
+      static(req, res);
+    }else if(path.extname(req.url) == '.png'){
+      static(req, res);
+    }
+  });
 
-	return server;
+  return server;
 };
 
 function index (res){
 
-	var issues = [];
+  var issues = [];
 
-	github.issues.getForRepo(options, function(err, datas) {
-		var done = 0;
-		Array.prototype.forEach.call(datas, function(data, index, array) {
-			var issue = {};
-			issue.num   = data.number;
-			issue.title = data.title;
-			issue.html_url = data.html_url;
-			client.fetch(data.html_url, {}, function(err, $, fres) {
-				var thumbup = $("button.btn-link.reaction-summary-item.tooltipped.tooltipped-se.tooltipped-multiline").text();
-				thumbup = thumbup.replace(/(^\s+)|(\s+$)/g, "");
-				var reg = /\d+/;
-				var num = 0;
-				if (reg.test(thumbup)) {
-					num = reg.exec(thumbup);
-				}
-				issue.thumbup = num;
-				issues.push(issue);
-				done++;
-				if (done === array.length) {
-					issues.sort(function(a, b){
-						if ((a.thumbup * 1) < (b.thumbup * 1)) return 1;
-						if ((a.thumbup * 1) > (b.thumbup * 1)) return -1;
-						return 0;
-					});
-					var html = nunjucks.render('index.html', { issues: issues });
-					res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
-					res.end(html);
-				}
-			});
-		});
-	});
+  github.issues.getForRepo(options, function(err, datas) {
+    var done = 0;
+    Array.prototype.forEach.call(datas, function(data, index, array) {
+      var issue = {};
+      issue.num   = data.number;
+      issue.title = data.title;
+      issue.html_url = data.html_url;
+      client.fetch(data.html_url, {}, function(err, $, fres) {
+        var thumbup = $("button.btn-link.reaction-summary-item.tooltipped.tooltipped-se.tooltipped-multiline").text();
+        thumbup = thumbup.replace(/(^\s+)|(\s+$)/g, "");
+        var reg = /\d+/;
+        var num = 0;
+        if (reg.test(thumbup)) {
+          num = reg.exec(thumbup);
+        }
+        issue.thumbup = num;
+        issues.push(issue);
+        done++;
+        if (done === array.length) {
+          issues.sort(function(a, b){
+            if ((a.thumbup * 1) < (b.thumbup * 1)) return 1;
+            if ((a.thumbup * 1) > (b.thumbup * 1)) return -1;
+            return 0;
+          });
+          var html = nunjucks.render('index.html', { issues: issues });
+          res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
+          res.end(html);
+        }
+      });
+    });
+  });
 }
 
 function static (req, res){
-	var mimeTypes = {
-		'.js': 'text/javascript',
-		'.css': 'text/css',
-		'.png': 'image/png'
-	};
+  var mimeTypes = {
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.png': 'image/png'
+  };
 
-	var filepath = './public' + path.dirname(req.url) + '/' + path.basename(req.url);
+  var filepath = './public' + path.dirname(req.url) + '/' + path.basename(req.url);
 
-	fs.readFile(filepath, function(err, data){
-		if(err){
-			res.writeHead(500);
-			return res.end('Error loading file');
-		}
-		res.writeHead(200, {
-			'Content-Type': mimeTypes[path.extname(req.url)]
-		});
-		res.end(data);
-	});
+  fs.readFile(filepath, function(err, data){
+    if(err){
+      res.writeHead(500);
+      return res.end('Error loading file');
+    }
+    res.writeHead(200, {
+      'Content-Type': mimeTypes[path.extname(req.url)]
+    });
+    res.end(data);
+  });
 }
